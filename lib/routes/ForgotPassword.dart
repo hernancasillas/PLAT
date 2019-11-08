ecd1386e48cc79b510ffd3b06762765adbd4184e
Joaquin Garcia <joa_mks@hotmail.com>
2019-11-07 23:54:23 -0600

    forgotpassword

--- lib/routes/ForgotPassword.dart
+++ lib/routes/ForgotPassword.dart
@@ -1,10 +1,15 @@
 import 'package:examen_flutter/main.dart';
 import 'package:examen_flutter/widgets/drawer.dart';
+import 'package:examen_flutter/widgets/raisedgradbutton.dart';
 import 'package:flutter/material.dart';
 import 'package:gradient_app_bar/gradient_app_bar.dart';
 
+
+final email = TextEditingController();
+TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+ 
 class ForgotPassword extends StatelessWidget {
-  static const String routeName = '/routes/Vendedores';
+  static const String routeName = '/routes/ForgotPassword';
 
   @override
   Widget build(BuildContext context) {
@@ -17,25 +22,83 @@ class ForgotPassword extends StatelessWidget {
                 end: Alignment.bottomCenter,
                 colors: [const Color(0xFF064B71), const Color(0xFF2692C2)],
               ),
-              actions: <Widget>[
-            // action button
-            IconButton(
-              icon: Icon(Icons.home),
-              onPressed: () {
-                 Navigator.of(context).push(MaterialPageRoute(
-                  builder: (BuildContext context) => MainScreen()));
-              },
-            ),
-          ],
+              
       ),
-        drawer: AppDrawer(),
-        body: Center(
-          child:Column(
-            children:<Widget>[
-              Text("Plantilla"),
-            ],
-          ),
-          )
+        
+        body: SnackBarPage(),
       );
   }
 }
+
+class SnackBarPage extends StatelessWidget {
+    final email = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final emailField = TextField(
+          style: style,
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+              hintText: "Email",
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+              controller: email,
+        );
+   
+   
+      final sendButton = RaisedGradientButton(
+            gradient: LinearGradient(
+            colors: <Color>[Color(0xff01ac4d3),Color(0xff0299cce),]
+          ),
+            onPressed: () {
+               if(email.text=='')
+               return showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      // Retrieve the text the that user has entered by using the
+                      // TextEditingController.
+                      content: Text('Email empty'),
+                    );
+                  },
+                );
+
+               else
+                  return showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      // Retrieve the text the that user has entered by using the
+                      // TextEditingController.
+                      content: Text('Recovery email sent'),
+                    );
+                  },
+                ); 
+            },
+            child: Text("SEND ME RESET PASSWORD",
+                textAlign: TextAlign.center,
+                style: style.copyWith(
+                    color: Colors.white, fontWeight: FontWeight.bold)),
+        );
+    return Center(
+           child: Container(
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(36.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Enter your email address below and we'll get "),Text("you back on the track"),
+                    SizedBox(height: 50.0),
+                    emailField,
+                    SizedBox(height: 20.0),
+                    sendButton,
+                  ],
+                ),
+              ),
+            )
+        );
+  }
+}
+
