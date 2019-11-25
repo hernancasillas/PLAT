43ff36e1c02938c92629ed99a1e8e4a9ac51ebcc
Joaquin Garcia <joa_mks@hotmail.com>
2019-11-25 17:48:47 -0600

    gopremium1.6

--- lib/routes/GoPremium.dart
+++ lib/routes/GoPremium.dart
@@ -3,10 +3,92 @@ import 'package:examen_flutter/widgets/drawer.dart';
 import 'package:examen_flutter/wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:gradient_app_bar/gradient_app_bar.dart';
+import 'package:examen_flutter/widgets/raisedgradbutton.dart';
+bool visaPres=false;
+bool masterPres=false;
+bool oxxoPres=false;
+bool americanPres=false;
 
+final _formKey = GlobalKey<FormState>();
+
+final style =  new TextStyle(fontFamily: 'Montserrat');
+ final buyPremium = RaisedGradientButton(
+      width: 150,
+          gradient: LinearGradient(
+            colors: <Color>[Color(0xff01ac4d3),Color(0xff0299cce),]
+          ),
+          
+           onPressed: () {
+             if (_formKey.currentState.validate()) {
+                  
+            
+             }
+              
+            },
+          child: Text("Buy Premium",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+              color: Colors.white, fontWeight: FontWeight.bold)),
+        );
+  
+    final numberField = TextFormField(
+          
+          style: style,
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+              hintText: "Card Number",
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+          validator: (val) => val.length < 10 ? 'Numero Correcto' : null,
+          onChanged: (val) {
+             // setState(() => pssswd = val);
+          },
+        );
+
+     final dateField = TextFormField(
+          
+          style: style,
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+              hintText: "MM/AA",
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+          validator: (val) => val.length < 10 ? 'Numero Correcto' : null,
+          onChanged: (val) {
+             // setState(() => passwd = val);
+          },
+        );
+      final cvvField = TextFormField(
+          
+          style: style,
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+              hintText: "CVV",
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+          validator: (val) => val.length < 10 ? 'Numero Correcto' : null,
+          onChanged: (val) {
+             // setState(() => passwd = val);
+          },
+        );
+
+         final nameField = TextFormField(
+          
+          style: style,
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+              hintText: "Card Owner",
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+          validator: (val) => val.length < 10 ? 'Numero Correcto' : null,
+          onChanged: (val) {
+             // setState(() => passwd = val);
+          },
+        );
 class GoPremium extends StatelessWidget {
-  static const String routeName = '/routes/Vendedores';
 
+  static const String routeName = '/routes/GoPremium';
+  
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -33,22 +115,191 @@ class GoPremium extends StatelessWidget {
 
 
         body: Center(
-          child:Column(
-            children:<Widget>[
-              Row(children: <Widget>[
-                Container(color: Colors.grey,height: 50,width: 50,margin: const EdgeInsets.all(10.0)),
-                Container(color: Color(0x6E6B6B))
+          
+          child: Column(children: <Widget>[
+            
+            BodyGoPremium(),
+            MyCustomForm(),
+            
 
 
-              ],),
-              Row(children: <Widget>[
-
-
-              ],),
-              Text("HOLI"),
-            ],
-          ),
+          ],),
           )
       );
+  }
+}
+
+
+class MyCustomForm extends StatefulWidget {
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+class MyCustomFormState extends State<MyCustomForm> {
+  // Crea una clave global que identificará de manera única el widget Form
+  // y nos permita validar el formulario
+  //
+  // Nota: Esto es un GlobalKey<FormState>, no un GlobalKey<MyCustomFormState>!
+
+  @override
+  Widget build(BuildContext context) {
+    // Crea un widget Form usando el _formKey que creamos anteriormente
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+              width:MediaQuery.of(context).size.width/1.4 ,child:numberField
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.center,children: <Widget>[
+              Container(child: cvvField, width:MediaQuery.of(context).size.width/3,margin: const EdgeInsets.all(10.0)),
+              Container(child: dateField, width:MediaQuery.of(context).size.width/3,margin: const EdgeInsets.all(10.0))
+            ],),
+            Container(
+              width:MediaQuery.of(context).size.width/1.4 ,child:nameField,margin:const EdgeInsets.only(bottom: 10)
+            ),
+            buyPremium,
+        ],
+      ),
+    );
+  }
+}
+
+
+class BodyGoPremium extends StatefulWidget {
+  
+  @override
+  _BodyGoPremium createState() => _BodyGoPremium();
+}
+
+
+
+class _BodyGoPremium extends State<BodyGoPremium> {
+ 
+  // Variables iniciales
+  static Color colorNoPress=Colors.grey;
+  static Color colorPress=Colors.black;
+
+
+  Color colorVisa=colorNoPress;
+  Color colorMaster=colorNoPress;
+  Color colorOxxo=colorNoPress;
+  Color colorAmerican=colorNoPress;
+  //visapress =1
+//master= 2
+//oxxo press=3
+//american press=4; 
+  void _toggleVisa() {
+    setState(() {
+      visaPres=true;
+        masterPres=false;
+        oxxoPres=false;
+        americanPres=false;
+
+        colorVisa=colorPress;
+        colorMaster=colorNoPress;
+        colorOxxo=colorNoPress;
+        colorAmerican=colorNoPress;
+    });
+  }
+  void _toggleMaster() {
+    setState(() {
+            visaPres=false;
+            masterPres=true;
+            oxxoPres=false;
+            americanPres=false;
+
+            colorVisa=colorNoPress;
+            colorMaster=colorPress;
+            colorOxxo=colorNoPress;
+            colorAmerican=colorNoPress;
+    });
+  }
+  void _toggleOxxo() {
+    setState(() {
+              visaPres=false;
+              masterPres=false;
+              oxxoPres=true;
+              americanPres=false;
+
+              colorVisa=colorNoPress;
+              colorMaster=colorNoPress;
+              colorOxxo=colorPress;
+              colorAmerican=colorNoPress;
+    });
+  }
+
+  void _toggleAmerican() {
+    setState(() {
+      visaPres=false;
+                masterPres=false;
+                oxxoPres=false;
+                americanPres=true;
+
+                colorVisa=colorNoPress;
+                colorMaster=colorNoPress;
+                colorOxxo=colorNoPress;
+                colorAmerican=colorPress;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+      return Column(
+            children:<Widget>[
+              Row(mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    GestureDetector(
+                    child: Container(decoration: new BoxDecoration(borderRadius: BorderRadius.circular(10.0),color: colorVisa)
+                    ,height: 100,width: MediaQuery.of(context).size.width/3,margin: const EdgeInsets.all(10.0),
+                    child: Image.asset("assets/logo_visa.png", fit: BoxFit.contain,),),
+                    onTap:_toggleVisa,
+                    ),
+                    GestureDetector(
+                    child: Container(decoration: new BoxDecoration(borderRadius: BorderRadius.circular(10.0),color: colorMaster)
+                    ,height: 100,width: MediaQuery.of(context).size.width/3,margin: const EdgeInsets.all(10.0),
+                    child: Image.asset("assets/logo_mastercard.png", fit: BoxFit.contain,),
+                    ),
+                    onTap:_toggleMaster,
+                  )
+               
+               
+
+
+              ],
+              ),
+              
+
+
+
+
+
+              Row(mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  GestureDetector(
+                    child: Container(decoration: new BoxDecoration(borderRadius: BorderRadius.circular(10.0),color: colorOxxo),
+                    height: 100,width: MediaQuery.of(context).size.width/3,margin: const EdgeInsets.all(10.0),
+                    child: Image.asset("assets/logo_oxxo.png", fit: BoxFit.contain,),
+                    ),
+                    onTap:_toggleOxxo,
+                    ),
+                    GestureDetector(
+                    child: Container(decoration: new BoxDecoration(borderRadius: BorderRadius.circular(10.0),color: colorAmerican),
+                    height: 100,width: MediaQuery.of(context).size.width/3,margin: const EdgeInsets.all(10.0),
+                    child: Image.asset("assets/logo_americanexpress.png", fit: BoxFit.contain,),
+                    ),
+                    onTap:_toggleAmerican,
+                    )
+                  
+
+
+
+              ],),
+              Text("ole xd"),
+            ],
+          );
+             
+      
   }
 }
