f95faba31158445b6de78ec4fc0f055f25910965
Hernan Casillas <hernancasillas@gmail.com>
2019-11-26 17:51:19 -0600

    Update

--- lib/routes/ViewRecipe.dart
+++ lib/routes/ViewRecipe.dart
@@ -1,14 +1,17 @@
+import 'dart:developer';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 /* import 'package:examen_flutter/genShoppingList.dart';
 import 'package:examen_flutter/main.dart'; */
 import 'package:examen_flutter/widgets/drawer.dart';
 import 'package:examen_flutter/widgets/loading.dart';
+import 'package:examen_flutter/widgets/raisedgradbutton.dart';
 import 'package:examen_flutter/wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:gradient_app_bar/gradient_app_bar.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
-class ViewRecipe extends StatelessWidget {
+class ViewRecipe extends StatefulWidget {
   static const String routeName = '/routes/ViewRecipe';
 
   final String image;
@@ -17,14 +20,21 @@ class ViewRecipe extends StatelessWidget {
   final String rating;
   final String steps;
   final int recipeId;
-
-  ViewRecipe({Key key, @required this.user, @required this.image, @required this.titulo, @required this.rating, @required this.steps, @required this.recipeId}) : super(key: key);
   
 
+  ViewRecipe({Key key, @required this.user, @required this.image, @required this.titulo, @required this.rating, @required this.steps, @required this.recipeId}) : super(key: key);
+
+  @override
+  _ViewRecipeState createState() => _ViewRecipeState();
+}
+
+class _ViewRecipeState extends State<ViewRecipe> {
+  var rating2;
   @override
   Widget build(BuildContext context) {
+    TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 15.0);
     return StreamBuilder(
-      stream: Firestore.instance.collection("ingrecipes").document(user.uid).collection(user.uid).snapshots(),
+      stream: Firestore.instance.collection("ingrecipes").document(widget.user.uid).collection(widget.user.uid).snapshots(),
       builder: (context, snapshot1){
         if(!snapshot1.hasData)
         {
@@ -50,9 +60,9 @@ class ViewRecipe extends StatelessWidget {
             ),
           ],
       ),
-        drawer: AppDrawer(user: user),
+        drawer: AppDrawer(user: widget.user),
         body:StreamBuilder( 
-          stream: Firestore.instance.collection('recipes').document(user.uid).collection(user.uid).document('1').snapshots(),
+          stream: Firestore.instance.collection('recipes').document(widget.user.uid).collection(widget.user.uid).document('1').snapshots(),
           builder: (context, snapshot) {
 
             if(!snapshot.hasData)
@@ -69,18 +79,21 @@ class ViewRecipe extends StatelessWidget {
                   Padding(
                     padding: const EdgeInsets.fromLTRB(100, 20, 15, 0),
                     child:              
-                      Text(titulo, style:TextStyle(fontSize: 25),),
+                      Text(widget.titulo, style:TextStyle(fontSize: 25),),
                   ),
                   Padding(
                     padding: const EdgeInsets.fromLTRB(95,5,0,0),
                     child:
                   FlutterRatingBar(
-                      initialRating: double.parse(rating),
+                      initialRating: double.parse(widget.rating),
                       fillColor: Colors.amber,
                       borderColor: Colors.amber.withAlpha(50),
                       allowHalfRating: true,
                       onRatingUpdate: (rating) {
                           print(rating);
+                          setState(() {
+                            rating2 = rating;
+                          });
                       },
                     ),
                   ),
@@ -94,7 +107,7 @@ class ViewRecipe extends StatelessWidget {
                           SizedBox(
                             height: 100,
                             child:  Image.asset(
-                              image,
+                              widget.image,
                             ),
                           ) 
                           
@@ -111,7 +124,7 @@ class ViewRecipe extends StatelessWidget {
                     height: 100,
                     
                     child:
-                    Column(children:getIngredients(snapshot1, context, this.recipeId),),
+                    Column(children:getIngredients(snapshot1, context, this.widget.recipeId),),
                   ),
                   SizedBox(height: 30.0),
                   Padding(
@@ -123,9 +136,30 @@ class ViewRecipe extends StatelessWidget {
                     padding: const EdgeInsets.fromLTRB(20, 5, 15, 0),
                     child:
                     //Text("1. In a large mixing bowl, create a well with flour then add eggs, slowly whisking them into flour. Add sugar and salt and stir until combined. Gradually add the milk, whisking to combine.\n Let batter stand at room temperature until bubbly on top, 15 to 20 minutes.\n\n2.In a small skillet over medium heat, melt butter. About 1/4 cup at a time, drop batter evenly onto pan, swirling it to evenly coat.\n\n3. Wait for 2 minutes, then flip and cook 1 minute more; repeat with remaining batter. Serve crêpes warm with fresh fruit and powdered sugar.",style:TextStyle(fontSize: 16), textAlign: TextAlign.left,),
-                    Text(steps,style:TextStyle(fontSize: 16), textAlign: TextAlign.left,),
+                    Text(widget.steps,style:TextStyle(fontSize: 16), textAlign: TextAlign.left,),
                   ),
                   SizedBox(height: 50.0),
+                   RaisedGradientButton(
+                      width: 150,
+                      
+                      gradient: LinearGradient(
+                        colors: <Color>[Color(0xff01ac4d3),Color(0xff0299cce),]
+                      ),
+                      onPressed: () {
+                            
+                              //return await myIngRecCollection.add({'cadena': sentence, 'recipeid': recid});
+                              log('Agregando: ' + rating2.round().toString() + 'con id: ' + widget.recipeId.toString() +  ' para ' + widget.user.uid);
+                              Firestore.instance.collection('recipes').document(widget.user.uid).collection(widget.user.uid).document(widget.recipeId.toString())
+                              .updateData({
+                                'rating': rating2.round().toString(),
+                              });
+                      },
+                      child: Text("Save",
+                        textAlign: TextAlign.center,
+                        style: style.copyWith(
+                        color: Colors.white, fontWeight: FontWeight.bold)
+                      ),          
+                    ),
                 ],
               ),
               ],
@@ -138,6 +172,7 @@ class ViewRecipe extends StatelessWidget {
       }
     );
   }
+
 getIngredients(AsyncSnapshot<QuerySnapshot> snapshot, context, recipeid){
     var ings = new List<DocumentSnapshot>();
     var docs = snapshot.data.documents;
